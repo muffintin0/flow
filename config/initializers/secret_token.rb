dc7e0321f5628de6509f4c72fe7c4a0a40f1bde7
muffintin0 <hmchem@gmail.com>
2013-01-26 18:05:58 -0500

    fresh app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flow::Application.config.secret_token = 'ba400c5cb4bafd19f5f3845b09fde66d8164503f837ebb46187e0c3fdccccc6d55a626b355b9ca2470359010a7b362cce2550605660fadd4b7351ec7fcfd3526'
