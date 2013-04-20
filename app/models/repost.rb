1d02d8b6d2f2389267fbbaa5709aeeba49ad30f4
muffintin0 <hmchem@gmail.com>
2013-04-20 12:27:37 -0400

    initial commit

--- app/models/repost.rb
+++
@@ -1,9 +0,0 @@
-class Repost < ActiveRecord::Base
-  attr_accessible :original_post_id, :repost_id
-
-  belongs_to :original_post, class_name: 'Micropost'
-  belongs_to :repost, class_name: 'Micropost'
-
-  validates :originial_post_id, presence: true
-  validates :repost_id, presence: true
-end
